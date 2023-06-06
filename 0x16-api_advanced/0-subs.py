27d72627c2c62cf71f25ba44f905e9bddaff9e1e
tomutanyi <tomutanyi@gmail.com>
2023-06-06 04:50:25 -0700

    task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Query a subreddit 
+"""
+
+from requests import get
+from sys import argv
+
+
+headers = {
+    "User-Agent": "Of course I had to use a custom User-Agent",
+    "X-Forwared-For": "iamthecavalry"
+}
+
+
+def number_of_subscribers(subreddit: str) -> int:
+    """
+    Query the subreddit
+    """
+    response = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+                   headers=headers)
+    data = response.json()
+    try:
+        if 'error' in data.keys():
+            return 0
+        else:
+            return data['data']['subscribers']
+    except Exception as e:
+        return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers(argv[1]))
